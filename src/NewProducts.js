b53e5a1b32050360ca2b77b01b79b11db833bc31
Bader Alsabah <bader@badercodes.com>
2022-07-05 14:39:30 +0400

    got 3 cards to appear side by side

--- src/NewProducts.js
+++ src/NewProducts.js
@@ -97,6 +97,120 @@ function NewProducts(props) {
                 </div>
               </div>
             </div>
+            <div className="col-md-4">
+              <div className="products-tabs">
+                {/* <!-- tab --!>*/}
+                {/* <div id="tab1" className="tab-pane active"> */}
+                {/* <div className="products-slick" data-nav="#slick-nav-1"> */}
+                {/* <!-- product 1 --!>*/}
+                <div className="product">
+                  <div className="product-img">
+                    <img src={`${props.ProductImg1}`} alt="" />
+                    <div className="product-label">
+                      {props.ProductSale1 && (
+                        <span className="sale">{props.ProductSale1}</span>
+                      )}
+                      <span className="new">NEW</span>
+                    </div>
+                  </div>
+                  <div className="product-body">
+                    <p className="product-category">{props.ProductCategory1}</p>
+                    <h3 className="product-name">
+                      <a href="#">{props.ProductName1}</a>
+                    </h3>
+                    <h4 className="product-price">
+                      {props.ProductPrice1}{" "}
+                      <del className="product-old-price">
+                        {props.ProductOldPrice1}
+                      </del>
+                    </h4>
+                    <div className="product-rating">
+                      <i className="fa fa-star"></i>
+                      <i className="fa fa-star"></i>
+                      <i className="fa fa-star"></i>
+                      <i className="fa fa-star"></i>
+                      <i className="fa fa-star"></i>
+                    </div>
+                    <div className="product-btns">
+                      <button className="add-to-wishlist">
+                        <i className="fa fa-heart-o"></i>
+                        <span className="tooltipp">add to wishlist</span>
+                      </button>
+                      <button className="add-to-compare">
+                        <i className="fa fa-exchange"></i>
+                        <span className="tooltipp">add to compare</span>
+                      </button>
+                      <button className="quick-view">
+                        <i className="fa fa-eye"></i>
+                        <span className="tooltipp">quick view</span>
+                      </button>
+                    </div>
+                  </div>
+                  <div className="add-to-cart">
+                    <button className="add-to-cart-btn">
+                      <i className="fa fa-shooping-cart"></i> add to cart
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="products-tabs">
+                {/* <!-- tab --!>*/}
+                {/* <div id="tab1" className="tab-pane active"> */}
+                {/* <div className="products-slick" data-nav="#slick-nav-1"> */}
+                {/* <!-- product 1 --!>*/}
+                <div className="product">
+                  <div className="product-img">
+                    <img src={`${props.ProductImg1}`} alt="" />
+                    <div className="product-label">
+                      {props.ProductSale1 && (
+                        <span className="sale">{props.ProductSale1}</span>
+                      )}
+                      <span className="new">NEW</span>
+                    </div>
+                  </div>
+                  <div className="product-body">
+                    <p className="product-category">{props.ProductCategory1}</p>
+                    <h3 className="product-name">
+                      <a href="#">{props.ProductName1}</a>
+                    </h3>
+                    <h4 className="product-price">
+                      {props.ProductPrice1}{" "}
+                      <del className="product-old-price">
+                        {props.ProductOldPrice1}
+                      </del>
+                    </h4>
+                    <div className="product-rating">
+                      <i className="fa fa-star"></i>
+                      <i className="fa fa-star"></i>
+                      <i className="fa fa-star"></i>
+                      <i className="fa fa-star"></i>
+                      <i className="fa fa-star"></i>
+                    </div>
+                    <div className="product-btns">
+                      <button className="add-to-wishlist">
+                        <i className="fa fa-heart-o"></i>
+                        <span className="tooltipp">add to wishlist</span>
+                      </button>
+                      <button className="add-to-compare">
+                        <i className="fa fa-exchange"></i>
+                        <span className="tooltipp">add to compare</span>
+                      </button>
+                      <button className="quick-view">
+                        <i className="fa fa-eye"></i>
+                        <span className="tooltipp">quick view</span>
+                      </button>
+                    </div>
+                  </div>
+                  <div className="add-to-cart">
+                    <button className="add-to-cart-btn">
+                      <i className="fa fa-shooping-cart"></i> add to cart
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
           {/* <!-- product 1 --!>*/}
           {/* <!-- product 2 --!>*/}
